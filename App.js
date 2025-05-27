b4ef5371c2b9eaf93b205b75940f4cd1fa3c898c
Leticia3179 <leticiasampaio170305@gmail.com>
2025-05-26 22:47:19 -0300

    26.05

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import Exemplo03 from './src/exemplos/exemplo-03';
 import Exemplo04 from './src/exemplos/exemplo-04';
 import Exemplo05 from './src/exemplos/exemplo-05';
 import Exemplo7 from './src/exemplos/exemplo-07';
+import Exemplo8 from './src/exemplos/exemplo-08';
+
 
 
 
@@ -24,6 +26,7 @@ import Atividade03 from './src/atividade/Atividade-03';
 import Atividade04 from './src/atividade/Atividade-04';
 import Atividade05 from './src/atividade/atividade-05';
 import Atividade06 from './src/atividade/atividade-06';
+import Atividade07 from './src/atividade/atividade-07';
  
 
 
@@ -31,7 +34,7 @@ import Atividade06 from './src/atividade/atividade-06';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo7/>
+      <Atividade07/>
       <StatusBar style="auto" />
     </View>
   );
